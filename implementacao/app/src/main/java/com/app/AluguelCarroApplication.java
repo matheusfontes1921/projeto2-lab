a61e3dec5ceb5b1717db81a1e6d73dc7e88f0cb7
André Avelar Moriya Sathler <andremoriya.sathler@gmail.com>
2023-03-22 12:53:45 -0300

    first commit - projeto

---
+++ implementacao/app/src/main/java/com/app/AluguelCarroApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguelCarroApplication.class, args);
+	}
+
+}
